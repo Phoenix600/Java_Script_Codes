0424ba56e3e6057af5fc44c44c9135de43652cbb
phoenix600 <pranayramteke613@gmail.com>
2022-11-18 15:25:11 +0530

    Added new code filesw

---
+++ Chapter-03/Random.js
@@ -0,0 +1,10 @@
+var min = Math.random();
+console.log(min);
+
+var min = 100000;
+var max = 999999;
+
+var random = Math.floor(Math.random() * (max-min +1)) + min;
+console.log(random);
+
+ var otp = Math.floor(Math.random() *(max - min) + 1) + min;
